bd896b78cf149992962a57e0de8243f1923c64f4
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-11-16 00:30:18 +0200

    User Dashboard

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Footer from "./components/utility/Footer";
 import ProductDetailsPage from "./pages/Products/ProductDetailsPage";
 import CartPage from "./pages/Cart/CartPage";
 import ChoosePayMethoudPage from "./pages/Checkout/ChoosePayMethoudPage";
+// admin pages
 import AdminAllProductsPage from "./pages/Admin/AdminAllProductsPage";
 import AdminAllOrdersPage from "./pages/Admin/AdminAllOrdersPage";
 import AdminAddProductsPage from "./pages/Admin/AdminAddProductsPage";
@@ -17,6 +18,14 @@ import AdminAddBrandPage from "./pages/Admin/AdminAddBrandPage";
 import AdminAddCategoryPage from "./pages/Admin/AdminAddCategoryPage";
 import AdminAddSubCategoryPage from "./pages/Admin/AdminAddSubCategoryPage";
 import AdminOrderDetailsPage from "./pages/Admin/AdminOrderDetailsPage";
+// user pages
+import UserAllOrdersPage from "./pages/User/UserAllOrdersPage";
+import UserFavoriteProductsPage from "./pages/User/UserFavoriteProductsPage";
+import UserAllAddresPage from "./pages/User/UserAllAddresPage";
+import UserAddAddressPage from "./pages/User/UserAddAddressPage";
+import UserEditAddressPage from "./pages/User/UserEditAddressPage";
+import UserProfilePage from "./pages/User/UserProfilePage";
+
 function App() {
   return (
     <div className="font">
@@ -39,6 +48,12 @@ function App() {
           <Route path="/admin/addcategory" element={<AdminAddCategoryPage />} />
           <Route path="/admin/addsubcategory" element={<AdminAddSubCategoryPage />}/>
           <Route path="/admin/orders/:id" element={<AdminOrderDetailsPage />} />
+          <Route path="/user/allorders" element={<UserAllOrdersPage />} />
+          <Route path="/user/favoriteproducts" element={<UserFavoriteProductsPage />} />
+          <Route path="/user/addresses" element={<UserAllAddresPage />} />
+          <Route path="/user/add-address" element={<UserAddAddressPage />} />
+          <Route path="/user/edit-address" element={<UserEditAddressPage />} />  
+          <Route path="/user/profile" element={<UserProfilePage />} />  
         </Routes>
       </BrowserRouter>
       <Footer />
