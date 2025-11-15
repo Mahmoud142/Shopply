df733a36a923d2d8bd11d97c91eabf5a5dd8ab16
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-11-15 16:10:44 +0200

    apply sideFilter & SearchCount

--- src/pages/Products/ShopProductPage.js
+++ src/pages/Products/ShopProductPage.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import CategoryHeader from '../../components/Category/CategoryHeader'
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import CategoryHeader from "../../components/Category/CategoryHeader";
+import SearchCountResult from "../../components/utility/SearchCountResult";
+import SideFilter from "../../components/utility/SideFilter";
+import CardProductsContainer from "../../components/Products/CardProductContainer";
+import Pagination from "../../components/utility/Pagination";
 const ShopProductPage = () => {
   return (
     <div>
       <CategoryHeader />
+      <Container>
+        <SearchCountResult title="جميع المنتجات" />
+        <Row className="d-flex flex-row">
+          <Col sm="2" xs="2" md="1" className="d-flex">
+            <SideFilter />
+          </Col>
+          <Col sm="10" xs="10" md="11">
+            <div className="d-flex flex-wrap">
+              <CardProductsContainer />
+            </div>
+          </Col>
+        </Row>
+        <Pagination />
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default ShopProductPage
+export default ShopProductPage;
