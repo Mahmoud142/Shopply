cf3418d5a25162779f1f4cafc75703670ec76072
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-11-30 23:15:13 +0200

    Update Path Params To PathText

--- src/components/utility/SubTitle.js
+++ src/components/utility/SubTitle.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Subtitle = ({ title, btntitle, path }) => {
+const Subtitle = ({ title, btntitle, pathText }) => {
   return (
     <div className="d-flex justify-content-between pt-4">
       <div className="sub-title">{title}</div>
-      <Link to={path}>
-        {btntitle ? <div className="shopping-now">{btntitle}</div> : null}
-      </Link>
+      {btntitle ? (
+        <Link to={`${pathText}`} style={{ textDecoration: "none" }}>
+          <div className="shopping-now">{btntitle}</div>
+        </Link>
+      ) : null}
     </div>
   );
 };
