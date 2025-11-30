8b8676151eea747ef5a3991c7b0eceac670e7011
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-11-30 23:21:53 +0200

    Use map and redux

--- src/components/Home/HomeCategory.js
+++ src/components/Home/HomeCategory.js
@@ -1,23 +1,34 @@
-import React from "react";
+// import React, { use } from "react";
 import { Container, Row } from "react-bootstrap";
 import Subtitle from "../utility/SubTitle";
 import CategoryCard from "../Category/CategoryCard";
-import img from "../../images/brand1.png";
-import img2 from "../../images/brand2.png";
-import img3 from "../../images/brand3.png";
-import img4 from "../../images/cart.png";
-import img5 from "../../images/cat2.png";
+import HomeCategoryHook from "../../hook/category/home-category-hook";
+import { Spinner } from "react-bootstrap";
 const HomeCategory = () => {
+  const [category, loading, colors] = HomeCategoryHook();
+
   return (
     <Container>
-      <Subtitle title="التصنيفات" btntitle="المزيد" path="/allcategory" />
+      <Subtitle title="التصنيفات" btntitle="المزيد" pathText="/allcategory" />
       <Row className="my-2 d-flex justify-content-between">
-        <CategoryCard title="الملابس" background="#f8f9fa" img={img} />
-        <CategoryCard title="الأحذية" background="#f8f9fa" img={img2} />
-        <CategoryCard title="الإكسسوارات" background="#f8f9fa" img={img3} />
-        <CategoryCard title="المجوهرات" background="#f8f9fa" img={img4} />
-        <CategoryCard title="حقائب اليد" background="#f8f9fa" img={img5} />
-        <CategoryCard title="حقائب الظهر" background="#f8f9fa" img={img} />
+        {loading === false ? (
+          category.data ? (
+            category.data.slice(0, 5).map((item, index) => {
+              return (
+                <CategoryCard
+                  key={index}
+                  title={item.name}
+                  img={item.image}
+                  background={colors[index]}
+                />
+              );
+            })
+          ) : (
+            <h4>لا يوجد تصنيفات</h4>
+          )
+        ) : (
+          <Spinner animation="border" variant="primary" />
+        )}
       </Row>
     </Container>
   );
