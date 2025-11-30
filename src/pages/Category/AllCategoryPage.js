3b45f06fc6432fa1b2f879a16fb204525cb85bda
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-11-30 23:24:00 +0200

    Update Category Page After using redux

--- src/pages/Category/AllCategoryPage.js
+++ src/pages/Category/AllCategoryPage.js
@@ -1,10 +1,16 @@
+// import React from 'react'
 import CategoryContainer from "../../components/Category/CategoryContainer";
 import Pagination from "../../components/utility/Pagination";
+import AllCategoryHook from "../../hook/category/all-category-page-hook";
 const AllCategoryPage = () => {
+  const [category, loading, pageCount, getPage] = AllCategoryHook();
+
   return (
-    <div>
-      <CategoryContainer />
-      <Pagination />
+    <div style={{ minHeight: "670px" }}>
+      <CategoryContainer data={category.data} loading={loading} />
+      {pageCount > 1 ? (
+        <Pagination pageCount={pageCount} onPress={getPage} />
+      ) : null}
     </div>
   );
 };
