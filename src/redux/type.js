e32fc2cba1c0e92d8b0d1da887449836c637dc6b
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-12-05 22:31:00 +0200

    add brand types

--- src/redux/type.js
+++ src/redux/type.js
@@ -1,3 +1,10 @@
-export const GET_ALL_CATEGORY = "GET_ALL_CATEGORY"
-export const CREATE_CATEGORY = "CREATE_CATEGORY"
-export const GET_ERROR = "GET_ERROR"
+// Global Action Types
+export const GET_ERROR = "GET_ERROR";
+
+// Category Action Types
+export const GET_ALL_CATEGORY = "GET_ALL_CATEGORY";
+export const CREATE_CATEGORY = "CREATE_CATEGORY";
+
+// Brand Action Types
+export const GET_ALL_BRAND = "GET_ALL_BRAND";
+export const CREATE_BRAND = "CREATE_BRAND";
