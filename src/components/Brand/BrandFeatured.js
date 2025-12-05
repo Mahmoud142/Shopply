b6aa9dedeada39efb909235a2cec135c5b83b127
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-12-05 22:35:38 +0200

    Apply redux to brand

--- src/components/Brand/BrandFeatured.js
+++ src/components/Brand/BrandFeatured.js
@@ -1,23 +1,27 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Spinner, Row } from "react-bootstrap";
 import SubTiltle from "../utility/SubTitle";
 import BrandCard from "./BrandCard";
-import brand1 from "../../images/brand1.png";
-import brand2 from "../../images/brand2.png";
-import brand3 from "../../images/brand3.png";
+import HomeBrandHook from "../../hook/brand/home-brand-hook";
 
 const BrandFeatured = ({ title, btntitle }) => {
+  const [brand, loading] = HomeBrandHook();
+
   return (
     <Container>
-      <div className="admin-content-text mt-3 ">الماركات المميزة</div>
       <SubTiltle title={title} btntitle={btntitle} pathText="/allbrand" />
       <Row className="my-1 d-flex justify-content-between">
-        <BrandCard img={brand1} />
-        <BrandCard img={brand2} />
-        <BrandCard img={brand3} />
-        <BrandCard img={brand2} />
-        <BrandCard img={brand1} />
-        <BrandCard img={brand3} />
+        {loading === false ? (
+          brand.data ? (
+            brand.data.slice(0, 5).map((item, index) => {
+              return <BrandCard key={index} img={item.image} />;
+            })
+          ) : (
+            <h4>لا يوجد ماركات</h4>
+          )
+        ) : (
+          <Spinner animation="border" variant="primary" />
+        )}
       </Row>
     </Container>
   );
