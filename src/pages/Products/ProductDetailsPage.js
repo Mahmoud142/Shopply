164a5671a242350ee79aaf7e875f7f04b9ca2c46
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-12-11 00:07:45 +0200

    add container for similar products

--- src/pages/Products/ProductDetailsPage.js
+++ src/pages/Products/ProductDetailsPage.js
@@ -1,21 +1,25 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import CategoryHeader from '../../components/Category/CategoryHeader'
 import CardProductContainer from '../../components/Products/CardProductContainer'
 import ProductDetails from '../../components/Products/ProductDetalis'
 import RateContainer from '../../components/Rate/RateContainer'
-
+import ViewProductDetailsHook from '../../hook/product/view-product-details-hook'
 const ProductDetailsPage = () => {
+    const { id } = useParams();
+    const [item, images, cat, brand, prod] = ViewProductDetailsHook(id);
+    
     return (
-        <div style={{ minHeight: '670px' }}>
-            <CategoryHeader />
-            <Container>
-                <ProductDetails />
-                <RateContainer />
-                <CardProductContainer title="منتجات قد تعجبك" />
-            </Container>
-        </div>
-    )
+      <div style={{ minHeight: "670px" }}>
+        <CategoryHeader />
+        <Container>
+          <ProductDetails />
+          <RateContainer />
+          <CardProductContainer products={prod} title="منتجات قد تعجبك" />
+        </Container>
+      </div>
+    );
 }
 
 export default ProductDetailsPage
