f589dea213f6910444a76350985b2ab06ee24175
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-12-11 00:06:20 +0200

    apply GET-ONE-CATEGORY

--- src/redux/actions/categoryAction.js
+++ src/redux/actions/categoryAction.js
@@ -1,4 +1,9 @@
-import { GET_ALL_CATEGORY, GET_ERROR, CREATE_CATEGORY } from "../type";
+import {
+  GET_ALL_CATEGORY,
+  GET_ERROR,
+  CREATE_CATEGORY,
+  GET_ONE_CATEGORY,
+} from "../type";
 import useGetData from "../../hooks/useGetData";
 import { useInsertDataWithImage } from "../../hooks/useInsertData";
 //get all category
@@ -47,6 +52,21 @@ export const createCategory = (formData) => async (dispatch) => {
       type: CREATE_CATEGORY,
       payload: response,
       loading: true,
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_ERROR,
+      payload: "Error " + e,
+    });
+  }
+};
+
+export const getOneCategory = (id) => async (dispatch) => {
+  try {
+    const response = await useGetData(`/api/v1/categories/${id}`);
+    dispatch({
+      type: GET_ONE_CATEGORY,
+      payload: response,
     });
   } catch (e) {
     dispatch({
