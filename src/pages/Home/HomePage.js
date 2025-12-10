520ba8f975befb30c23fc2a30c9bd1591e81859c
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-12-10 21:09:09 +0200

    use redux

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -3,7 +3,11 @@ import HomeCategory from "../../components/Home/HomeCategory";
 import CardProductsContainer from "../../components/Products/CardProductContainer";
 import DiscountSection from "../../components/Home/DiscountSection";
 import BrandFeatured from "../../components/Brand/BrandFeatured";
+import ViewHomeProductsHook from "../../hook/product/view-home-products-hook";
 const HomePage = () => {
+  
+  const [homeProduct] = ViewHomeProductsHook();
+
   return (
     <div className="font" style={{ minHeight: "900px" }}>
       <Slider />
@@ -12,12 +16,14 @@ const HomePage = () => {
         title="الاكثر مبيعاً"
         btntitle="المزيد"
         path="/products"
+        products={homeProduct}
       />
       <DiscountSection />
       <CardProductsContainer
         title="الاعلي تقييما"
         btntitle="المزيد"
         path="/products"
+        products={homeProduct}
       />
       <BrandFeatured title="اشهر العلامات التجارية" btntitle="المزيد" />
     </div>
