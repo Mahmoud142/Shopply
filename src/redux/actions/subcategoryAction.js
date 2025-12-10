e4c25d427e56a5bdf7d3b3683d108824f0ce6499
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-12-10 16:55:20 +0200

    add getOneCategory

--- src/redux/actions/subcategoryAction.js
+++ src/redux/actions/subcategoryAction.js
@@ -1,6 +1,6 @@
-import { CREATE_SUBCATEGORY , GET_ERROR} from "../type";
-import {useInsertData} from "../../hooks/useInsertData";
-
+import { CREATE_SUBCATEGORY, GET_SUB_CATEGORY, GET_ERROR } from "../type";
+import { useInsertData } from "../../hooks/useInsertData";
+import  useGetData from "../../hooks/useGetData";
 // Create SubCategory Action
 export const createSubCategory = (formData) => async (dispatch) => {
   try {
@@ -14,6 +14,23 @@ export const createSubCategory = (formData) => async (dispatch) => {
     dispatch({
       type: GET_ERROR,
       payload: error.response.data,
+    });
+  }
+};
+//get sub category depend in cat id
+export const getOneCategory = (id) => async (dispatch) => {
+  try {
+    const response = await useGetData(`/api/v1/categories/${id}/subcategories`);
+
+    dispatch({
+      type: GET_SUB_CATEGORY,
+      payload: response,
+      loading: true,
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_ERROR,
+      payload: "Error " + e,
     });
   }
 };
