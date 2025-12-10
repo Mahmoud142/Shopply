6b3c6bdf3bbf12602e24e427a68f05bd53b58f9f
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-12-10 16:58:48 +0200

    Insert data hook

--- src/hooks/useInsertData.js
+++ src/hooks/useInsertData.js
@@ -1,18 +1,21 @@
-import baseUrl from '../Api/baseURL'
-
+import baseUrl from "../Api/baseURL";
 
 const useInsertDataWithImage = async (url, parmas) => {
-    const config={
-        headers:{"Content-Type":"multipart/form-data"}
-    }
-    const res = await baseUrl.post(url, parmas ,config);
-    console.log(res.status)
+  try {
+    const config = {
+      headers: { "Content-Type": "multipart/form-data" },
+    };
+    const res = await baseUrl.post(url, parmas, config);
+    console.log(res.status);
     return res;
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const useInsertData = async (url, parmas) => {
-    const res = await baseUrl.post(url, parmas);
-    return res;
-}
+  const res = await baseUrl.post(url, parmas);
+  return res;
+};
 
 export { useInsertData, useInsertDataWithImage };
