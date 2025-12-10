582d5c51855a4a29702deb2d4bea73a8584b1aad
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-12-11 00:06:55 +0200

    implement redux

--- src/components/Products/ProductGallery.js
+++ src/components/Products/ProductGallery.js
@@ -1,35 +1,30 @@
-import React from 'react'
+import React from "react";
 import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery from "react-image-gallery";
-import mobile from '../../images/mobile.png'
-import LeftButton from './LeftButton';
-import RightButton from './RightButton';
+import mobile from "../../images/mobile.png";
+import LeftButton from "./LeftButton";
+import RightButton from "./RightButton";
+import { useParams } from "react-router-dom";
+import ViewProductDetailsHook from "../../hook/product/view-product-details-hook";
 const ProductGallery = () => {
-    const images = [
-        {
-            original: `${mobile}`,
-        },
-        {
-            original: `${mobile}`,
-        },
-        {
-            original: `${mobile}`,
-        },
-    ];
-    return (
-        <div className="product-gallary-card d-flex justfiy-content-center  align-items-center
-        pt-2">
-            <ImageGallery items={images}
-                defaultImage={mobile}
-                showFullscreenButton={false}
-                isRTL={true}
-                showPlayButton={false}
-                showThumbnails={false}
-                renderRightNav={RightButton}
-                renderLeftNav={LeftButton}
-            />
-        </div>
-    )
-}
+  const { id } = useParams();
+  const [product, images, cat, brand] = ViewProductDetailsHook(id);
+  return (
+    <div
+      className="product-gallary-card d-flex justfiy-content-center  align-items-center
+        pt-2"
+    >
+      <ImageGallery
+        items={images}
+        showFullscreenButton={false}
+        isRTL={true}
+        showPlayButton={false}
+        showThumbnails={false}
+        renderRightNav={RightButton}
+        renderLeftNav={LeftButton}
+      />
+    </div>
+  );
+};
 
-export default ProductGallery
+export default ProductGallery;
