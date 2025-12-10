ee604c91777ca4040756d1695cae4b30455e32f4
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-12-10 22:20:21 +0200

    apply redux

--- src/pages/Products/ShopProductPage.js
+++ src/pages/Products/ShopProductPage.js
@@ -5,23 +5,31 @@ import SearchCountResult from "../../components/utility/SearchCountResult";
 import SideFilter from "../../components/utility/SideFilter";
 import CardProductsContainer from "../../components/Products/CardProductContainer";
 import Pagination from "../../components/utility/Pagination";
+import ViewSearchProductsHook from "../../hook/product/view-search-products-hook";
 const ShopProductPage = () => {
+  const [products, pagination, onPress] = ViewSearchProductsHook();
+  let pageCount = 0;
+  if (pagination) pageCount = pagination;
+
   return (
     <div>
       <CategoryHeader />
       <Container>
-        <SearchCountResult title="جميع المنتجات" />
+        <SearchCountResult title={`هناك ${products.length} نتيجة بحث`} />
         <Row className="d-flex flex-row">
           <Col sm="2" xs="2" md="1" className="d-flex">
             <SideFilter />
           </Col>
           <Col sm="10" xs="10" md="11">
-            <div className="d-flex flex-wrap">
-              <CardProductsContainer />
-            </div>
+            <CardProductsContainer
+              products={products}
+              title=""
+              btntitle=""
+              pathText="/products"
+            />
           </Col>
         </Row>
-        <Pagination />
+        <Pagination pageCount={pageCount} onPress={onPress} />
       </Container>
     </div>
   );
