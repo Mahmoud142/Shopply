10c05371ccb7fd7775da1e27659400549d1aa6dc
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-12-11 00:05:46 +0200

    apply GET-ONE-BRAND

--- src/redux/actions/brandAction.js
+++ src/redux/actions/brandAction.js
@@ -1,4 +1,4 @@
-import { GET_ALL_BRAND, GET_ERROR, CREATE_BRAND } from "../type";
+import { GET_ALL_BRAND, GET_ERROR, CREATE_BRAND, GET_ONE_BRAND } from "../type";
 import useGetData from "../../hooks/useGetData";
 import { useInsertDataWithImage } from "../../hooks/useInsertData";
 //get all brand
@@ -21,11 +21,26 @@ export const getAllBrand = (limit) => async (dispatch) => {
 //get all brand with pagination
 export const getAllBrandPage = (page) => async (dispatch) => {
   try {
-    const response = await useGetData(
-      `/api/v1/brands?limit=6&page=${page}`
-    );
+    const response = await useGetData(`/api/v1/brands?limit=6&page=${page}`);
     dispatch({
       type: GET_ALL_BRAND,
+      payload: response,
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_ERROR,
+      payload: "Error " + e,
+    });
+  }
+};
+
+//get one Brand
+export const getOneBrand = (id) => async (dispatch) => {
+  try {
+    const response = await useGetData(`/api/v1/brands/${id}`);
+
+    dispatch({
+      type: GET_ONE_BRAND,
       payload: response,
     });
   } catch (e) {
@@ -39,10 +54,7 @@ export const getAllBrandPage = (page) => async (dispatch) => {
 //get all brand with pagination
 export const createBrand = (formData) => async (dispatch) => {
   try {
-    const response = await useInsertDataWithImage(
-      `/api/v1/brands`,
-      formData
-    );
+    const response = await useInsertDataWithImage(`/api/v1/brands`, formData);
     dispatch({
       type: CREATE_BRAND,
       payload: response,
