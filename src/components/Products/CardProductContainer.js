966f4ababb9b4404403fbff74104b22affdb5268
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-12-10 21:13:43 +0200

    apply redux

--- src/components/Products/CardProductContainer.js
+++ src/components/Products/CardProductContainer.js
@@ -3,15 +3,18 @@ import { Container, Row } from "react-bootstrap";
 import SubTitle from "../utility/SubTitle";
 import ProductCard from "./ProductCard";
 
-const CardProductsContainer = ({ title, btntitle, path }) => {
+const CardProductsContainer = ({ title, btntitle, path, products }) => {
   return (
     <Container className="my-4">
-      <SubTitle title={title} btntitle={btntitle} path={path} />
-      <Row className="my-3 d-flex justify-content-between">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+      {products ? (
+        <SubTitle title={title} btntitle={btntitle} path={path} />
+      ) : null}
+      <Row className="my-2 d-flex justify-content-between">
+        {products
+          ? products.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))
+          : null}
       </Row>
     </Container>
   );
