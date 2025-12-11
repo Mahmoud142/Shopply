b34c4305ba7af74c71302ed2cb1d4a1a9f0494e5
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-12-12 00:12:08 +0200

    products mapping

--- src/components/Admin/AdminAllProducts.js
+++ src/components/Admin/AdminAllProducts.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import { Row } from 'react-bootstrap'
-import AdminAllProductsCard from './AdminAllProductsCard'
+import { Row } from "react-bootstrap";
+import AdminAllProductsCard from "./AdminAllProductsCard";
 
-const AdminAllProducts = () => {
-    return (
-        <div>
-            <div className='admin-content-text'>ادارة جميع المنتجات</div>
-            <Row className='justify-content-start'>
-                <AdminAllProductsCard />
-                <AdminAllProductsCard />
-                <AdminAllProductsCard />
-                <AdminAllProductsCard />
-                <AdminAllProductsCard />
-                <AdminAllProductsCard />
-                <AdminAllProductsCard />
-            </Row>
-            
-        </div>
-    )
-}
+const AdminAllProducts = ({ products }) => {
+  return (
+    <div>
+      <div className="admin-content-text">ادارة جميع المنتجات</div>
+      <Row className="justify-content-start">
+        {products && products.length >= 1 ? (
+          products.map((product, index) => {
+            return <AdminAllProductsCard key={index} product={product} />;
+          })
+        ) : (
+          <p>لا يوجد منتجات لعرضها</p>
+        )}
+      </Row>
+    </div>
+  );
+};
 
-export default AdminAllProducts
+export default AdminAllProducts;
