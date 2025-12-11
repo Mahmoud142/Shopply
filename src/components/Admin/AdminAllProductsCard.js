491c875970b1f0b769071d97ed4a341641521c36
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-12-12 00:12:42 +0200

    delete Product

--- src/components/Admin/AdminAllProductsCard.js
+++ src/components/Admin/AdminAllProductsCard.js
@@ -1,47 +1,100 @@
-import React from 'react'
-import { Col,Card,Row } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import prod1 from '../../images/prod1.png'
-const AdminAllProductsCard = () => {
-    return (
-        <Col xs="12" sm="6" md="5" lg="4" className="d-flex">
-            <Card
-                className="my-2"
-                style={{
-                    width: "100%",
-                    height: "350px",
-                    borderRadius: "8px",
-                    border: "none",
-                    backgroundColor: "#FFFFFF",
-                }}>
-                <Row className="d-flex justify-content-center px-2">
-                    <Col className=" d-flex justify-content-between">
-                        <div className="d-inline item-delete-edit">ازاله</div>
-                        <div className="d-inline item-delete-edit">تعديل</div>
-                    </Col>
-                </Row>
-                <Link to="/products/:id" style={{ textDecoration: "none" }}>
-                    <Card.Img style={{ height: "228px", width: "100%" }} src={prod1} />
-                    <Card.Body>
-                        <Card.Title>
-                            <div className="card-title">
-                                سود كربون ساعة يد ذكية بيب إس أسود كربون{" "}
-                            </div>
-                        </Card.Title>
-                        <Card.Text>
-                            <div className="d-flex justify-content-between">
-                                <div className="card-rate">4.5</div>
-                                <div className="d-flex">
-                                    <div className="card-currency mx-1">جنيه</div>
-                                    <div className="card-price">880</div>
-                                </div>
-                            </div>
-                        </Card.Text>
-                    </Card.Body>
-                </Link>
-            </Card>
-        </Col>
-    )
-}
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { Col, Card, Row, Button, Modal } from "react-bootstrap";
+import { deleteProduct } from "../../redux/actions/productAction";
 
-export default AdminAllProductsCard
+const AdminAllProductsCard = ({ product }) => {
+  const dispatch = useDispatch();
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleDelete = async () => {
+    await dispatch(deleteProduct(product._id));
+    setShow(false);
+    window.location.reload();
+  };
+  return (
+    <Col xs="12" sm="6" md="5" lg="4" className="d-flex">
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title>
+            {" "}
+            <div className="font">تاكيد الحذف</div>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="font">هل انت متاكد من عملية الحذف للمنتج</div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            className="font bg-success"
+            variant="success"
+            onClick={handleClose}
+          >
+            تراجع
+          </Button>
+          <Button
+            className="font bg-danger"
+            variant="dark"
+            onClick={handleDelete}
+          >
+            حذف
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Card
+        className="my-2"
+        style={{
+          width: "100%",
+          height: "350px",
+          borderRadius: "8px",
+          border: "none",
+          backgroundColor: "#FFFFFF",
+        }}
+      >
+        <Row className="d-flex justify-content-center px-2">
+          <Col className=" d-flex justify-content-between">
+            <div onClick={handleShow} className="d-inline item-delete-edit">
+              ازاله
+            </div>
+            <Link
+              to={`/admin/editproduct/${product._id}`}
+              style={{ textDecoration: "none" }}
+            >
+              <div className="d-inline item-delete-edit">تعديل</div>
+            </Link>
+          </Col>
+        </Row>
+        <Link
+          to={`/products/${product._id}`}
+          style={{ textDecoration: "none" }}
+        >
+          <Card.Img
+            style={{ height: "228px", width: "100%" }}
+            src={product.imageCover}
+          />
+          <Card.Body>
+            <Card.Title>
+              <div className="card-title">{product.title}</div>
+            </Card.Title>
+            <Card.Text>
+              <div className="d-flex justify-content-between">
+                <div className="card-rate">{product.ratingsQuantity}</div>
+                <div className="d-flex">
+                  <div className="card-currency mx-1">جنيه</div>
+                  <div className="card-price">{product.price}</div>
+                </div>
+              </div>
+            </Card.Text>
+          </Card.Body>
+        </Link>
+      </Card>
+    </Col>
+  );
+};
+
+export default AdminAllProductsCard;
