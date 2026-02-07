acd3a4d35bee00e2dd272e40d8f04c062ee8efd5
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-07 21:30:02 +0200

    feat(auth):new hook for forget password page

---
+++ src/hook/auth/forget-password-hook.js
@@ -0,0 +1,64 @@
+import  { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  // createNewUser,
+  forgetPassword,
+  // loginUser,
+} from "../../redux/actions/authAction";
+import { useNavigate } from "react-router-dom";
+import notify from "../useNotifaction";
+
+const ForgetPasswordHook = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const OnChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onSubmit = async () => {
+    if (email === "") {
+      notify("من فضلك ادخل الايميل", "error");
+      return;
+    }
+
+    localStorage.setItem("user-email", email);
+    setLoading(true);
+    await dispatch(
+      forgetPassword({
+        email,
+      }),
+    );
+    setLoading(false);
+  };
+
+  const res = useSelector((state) => state.authReducer.forgetPassword);
+
+  useEffect(() => {
+    if (loading === false) {
+      if (res) {
+        
+        if (res.data.status === "Success") {
+          notify("تم ارسال الكود للايميل بنجاح", "success");
+          setTimeout(() => {
+            navigate("/user/verify-code");
+          }, 1000);
+        }
+        else if (res.data.status === "fail") {
+          notify("هذا الحساب غير موجود لدينا", "error");
+        }
+        else {
+          notify(res.data.message, "error");
+        }
+      }
+      // console.log("res inside use effect",res);
+    }
+  }, [loading, res, navigate]);
+
+  return [OnChangeEmail, email, onSubmit];
+};
+
+export default ForgetPasswordHook;
