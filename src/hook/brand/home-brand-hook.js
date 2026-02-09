e5bb88096cf29c303827b1512e6f3822d778e0f5
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-09 17:33:06 +0200

    fix(brand hook): remove unwanted logs

--- src/hook/brand/home-brand-hook.js
+++ src/hook/brand/home-brand-hook.js
@@ -6,12 +6,12 @@ const HomeBrandHook = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getAllBrand());
-  }, []);
+  }, [dispatch]);
   //get last brand state from redux
   const brand = useSelector((state) => state.allBrand.brand);
   //get last loading state from redux
   const loading = useSelector((state) => state.allBrand.loading);
-  console.log(loading);
+  
   return [brand, loading];
 };
 export default HomeBrandHook;
