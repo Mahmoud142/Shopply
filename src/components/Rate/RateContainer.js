d83622d33761b6ee5f83d5262f6e158bdf93b672
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-10 17:08:59 +0200

    refactor(redux): Prop Drilling

--- src/components/Rate/RateContainer.js
+++ src/components/Rate/RateContainer.js
@@ -1,29 +1,51 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import rate from '../../images/rate.png'
-import Pagination from '../utility/Pagination';
-import RateItem from './RateItem';
-import RatePost from './RatePost';
-const RateContainer = () => {
-    return (
-        <Container className='rate-container'>
-            <Row>
-                <Col className="d-flex">
-                    <div className="sub-tile d-inline p-1 ">التقيمات</div>
-                    <img className="mt-2" src={rate} alt="" height="16px" width="16px" />
-                    <div className="cat-rate  d-inline  p-1 pt-2">4.3</div>
-                    <div className="rate-count d-inline p-1 pt-2">(160 تقييم)</div>
-                </Col>
-            </Row>
-            <RatePost />
-            <RateItem />
-            <RateItem />
-            <RateItem />
-            <RateItem />
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import rate from "../../images/rate.png";
+import Pagination from "../utility/Pagination";
+import RateItem from "./RateItem";
+import RatePost from "./RatePost";
+import { useParams } from "react-router-dom";
+import ViewAllReviewHook from "../../hook/review/view-all-review-hook";
+const RateContainer = ({ rateAvg, rateQty }) => {
+  const { id } = useParams();
+  const [allReview, onPress] = ViewAllReviewHook(id);
 
-            <Pagination />
-        </Container>
-    )
-}
+  return (
+    <Container className="rate-container">
+      <Row>
+        <Col className="d-flex">
+          <div className="sub-tile d-inline p-1 ">التقيمات</div>
+          <img className="mt-2" src={rate} alt="" height="16px" width="16px" />
+          <div className="cat-rate  d-inline  p-1 pt-2">{rateAvg}</div>
+          <div className="rate-count d-inline p-1 pt-2">
+            ({`${rateQty ?? 0}  تقيم`})
+          </div>
+        </Col>
+      </Row>
 
-export default RateContainer
+      <RatePost />
+
+      {allReview && allReview.data && allReview.data.length > 0 ? (
+        allReview.data?.map((review, index) => {
+          return <RateItem key={index} review={review} />;
+        })
+      ) : (
+        <h6>لا يوجد تقيمات الان</h6>
+      )}
+
+      {allReview && allReview.paginationResult &&
+      allReview.paginationResult.numberOfPages >= 2 ? (
+        <Pagination
+          pageCount={
+            allReview.paginationResult
+              ? allReview.paginationResult.numberOfPages
+              : 0
+          }
+          onPress={onPress}
+        />
+      ) : null}
+    </Container>
+  );
+};
+
+export default RateContainer;
