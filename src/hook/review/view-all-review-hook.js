c579b88e1cbddf401d7333b7e1e58f2bd6149367
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-10 17:05:15 +0200

    feat(review hook): new review hook for view all rates

---
+++ src/hook/review/view-all-review-hook.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { allReviewProduct } from "./../../redux/actions/reviewAction";
+
+const ViewAllReviewHook = (id) => {
+  const dispatch = useDispatch();
+
+  const [loading, setLoading] = useState(true);
+
+  const allReview = useSelector(
+    (state) => state.reviewReducer?.allReviewProduct,
+  );
+  console.log("allReview in hook:", allReview);
+  useEffect(() => {
+    setLoading(true);
+    dispatch(allReviewProduct(id, 1, 5));
+    setLoading(false);
+  }, [id, dispatch]);
+
+  const onPress = async (page) => {
+    await dispatch(allReviewProduct(id, page, 5));
+  };
+
+  return [allReview, onPress, loading];
+};
+
+export default ViewAllReviewHook;
