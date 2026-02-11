c16cef9606fa2cac6aa1cd47ae97a2ef7c178ca6
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-11 18:29:14 +0200

    refactor(format): format changes

--- src/pages/Products/ProductDetailsPage.js
+++ src/pages/Products/ProductDetailsPage.js
@@ -1,25 +1,38 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import CategoryHeader from '../../components/Category/CategoryHeader'
-import CardProductContainer from '../../components/Products/CardProductContainer'
-import ProductDetails from '../../components/Products/ProductDetalis'
-import RateContainer from '../../components/Rate/RateContainer'
-import ViewProductDetailsHook from '../../hook/product/view-product-details-hook'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import CategoryHeader from "../../components/Category/CategoryHeader";
+import CardProductContainer from "../../components/Products/CardProductContainer";
+import ProductDetails from "../../components/Products/ProductDetalis";
+import RateContainer from "../../components/Rate/RateContainer";
+import ViewProductDetailsHook from "../../hook/product/view-product-details-hook";
 const ProductDetailsPage = () => {
     const { id } = useParams();
-    const [item, images, cat, brand, prod] = ViewProductDetailsHook(id);
-    
-    return (
-      <div style={{ minHeight: "670px" }}>
-        <CategoryHeader />
-        <Container>
-          <ProductDetails />
-          <RateContainer />
-          <CardProductContainer products={prod} title="منتجات قد تعجبك" />
-        </Container>
-      </div>
-    );
-}
+    const [item, , , , prod] = ViewProductDetailsHook(id);
 
-export default ProductDetailsPage
+    let items = [];
+    if (prod) items = prod.slice(0, 4);
+
+    let rateAvg = 0;
+    let rateQty = 0;
+    if (item) {
+        rateAvg = item.ratingsAverage;
+        rateQty = item.ratingsQuantity;
+    }
+
+    return (
+        <div style={{ minHeight: "670px" }}>
+            <CategoryHeader />
+            <Container>
+                <ProductDetails />
+                <RateContainer rateAvg={rateAvg} rateQty={rateQty} />
+                <CardProductContainer
+                    products={items}
+                    title="منتجات قد تعجبك"
+                />
+            </Container>
+        </div>
+    );
+};
+
+export default ProductDetailsPage;
