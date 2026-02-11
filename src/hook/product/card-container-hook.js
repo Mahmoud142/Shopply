f9769e2dc2332508db7f075c0fa94994033ffc18
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-11 18:31:03 +0200

    feat(wishlist): Product container hook for wishlist

---
+++ src/hook/product/card-container-hook.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserWishList } from "./../../redux/actions/wishlistAction";
+
+const CardContainerHook = () => {
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(true);
+    const [favProd, setFavProd] = useState([]);
+
+    const res = useSelector((state) => state.addToWishListReducer.allWishList);
+
+    useEffect(() => {
+        const get = async () => {
+            setLoading(true);
+            await dispatch(getUserWishList());
+            setLoading(false);
+        };
+
+        get();
+    }, []);
+
+    useEffect(() => {
+        if (loading === false) {
+            if (res?.length >= 1) {
+                setFavProd(res.map((item) => item?._id));
+            } else setFavProd([]);
+        }
+    }, [loading]);
+
+    return [favProd];
+};
+
+export default CardContainerHook;
