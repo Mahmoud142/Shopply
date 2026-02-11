d53e26a273d0ce4bf4360a186937495e50e00928
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-11 18:33:50 +0200

    feat(redux): Apply wishlist states to jsx

--- src/components/User/UserFavoriteProduct.js
+++ src/components/User/UserFavoriteProduct.js
@@ -1,26 +1,45 @@
-import React from 'react'
-import { Row } from 'react-bootstrap';
-import ProductCard from './../Products/ProductCard';
-import Pagination from '../utility/Pagination'
+import { useEffect, useState } from "react";
+import { Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import CardProductsContainer from "../Products/CardProductContainer";
+import { getUserWishList } from "../../redux/actions/wishlistAction";
+
 const UserFavoriteProduct = () => {
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(true);
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        const get = async () => {
+            setLoading(true);
+            await dispatch(getUserWishList());
+            setLoading(false);
+        };
+        get();
+    }, [dispatch]);
+
+    const res = useSelector((state) => state.addToWishListReducer?.allWishList);
+
+    useEffect(() => {
+        if (loading === false) {
+            if (res) setItems(res);
+        }
+    }, [loading, res]);
     return (
         <div>
             <div className="admin-content-text pb-4">قائمة المفضلة</div>
-            <Row className='justify-content-start'>
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
+            <Row className="justify-content-start">
+                {items?.length <= 0 ? (
+                    <h6>لا يوجد منتجات مفضله حاليا</h6>
+                ) : (
+                    <CardProductsContainer
+                        products={res}
+                        title=""
+                        btntitle=""
+                    />
+                )}
             </Row>
-            <Pagination />
         </div>
-    )
-}
+    );
+};
 
-export default UserFavoriteProduct
+export default UserFavoriteProduct;
