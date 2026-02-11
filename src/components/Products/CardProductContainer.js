1f7d62d86cd3a8ed9ab09e9a10723d0b917005a1
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-11 18:40:41 +0200

    feat: show favourite products

--- src/components/Products/CardProductContainer.js
+++ src/components/Products/CardProductContainer.js
@@ -2,22 +2,35 @@ import React from "react";
 import { Container, Row } from "react-bootstrap";
 import SubTitle from "../utility/SubTitle";
 import ProductCard from "./ProductCard";
+import { ToastContainer } from "react-toastify";
+import CardContainerHook from "./../../hook/product/card-container-hook";
 
-const CardProductsContainer = ({ title, btntitle, products, pathText }) => {
-  return (
-    <Container className="my-4">
-      {products ? (
-        <SubTitle title={title} btntitle={btntitle} pathText={pathText} />
-      ) : null}
-      <Row className="my-2 d-flex justify-content-between">
-        {products
-          ? products.map((product) => (
-              <ProductCard key={product._id} product={product} />
-            ))
-          : null}
-      </Row>
-    </Container>
-  );
+const CardProductsContainer = ({ products, title, btntitle, pathText }) => {
+    const [favProd] = CardContainerHook();
+
+    return (
+        <Container className="my-4">
+            {products ? (
+                <SubTitle
+                    title={title}
+                    btntitle={btntitle}
+                    pathText={pathText}
+                />
+            ) : null}
+            <Row className="my-2 d-flex justify-content-between">
+                {products
+                    ? products.map((item, index) => (
+                          <ProductCard
+                              favProd={favProd}
+                              key={index}
+                              product={item}
+                          />
+                      ))
+                    : null}
+            </Row>
+            <ToastContainer />
+        </Container>
+    );
 };
 
 export default CardProductsContainer;
