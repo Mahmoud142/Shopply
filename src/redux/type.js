230beb4486d4bfd07ffbfe07aeba8d29f5ffcccf
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-16 17:21:59 +0200

    feat: new Coupon Types

--- src/redux/type.js
+++ src/redux/type.js
@@ -43,3 +43,10 @@ export const UPDATE_REVIEW = "UPDATE_REVIEW";
 export const ADD_TO_WISHLIST = "ADD_TO_WISHLIST";
 export const REMOVE_FROM_WISHLIST = "REMOVE_FROM_WISHLIST";
 export const USER_WISHLIST = "USER_WISHLIST";
+
+// Coupons
+export const ADD_COUPON = "ADD_COUPON";
+export const GET_ALL_COUPON = "GET_ALL_COUPON";
+export const DELETE_COUPON = "DELETE_COUPON";
+export const GET_ONE_COUPON = "GET_ONE_COUPON";
+export const EDIT_COUPON = "EDIT_COUPON";
