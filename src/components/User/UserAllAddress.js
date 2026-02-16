c1376a7c0b16d87545d6eb49fcbfeed2a92259b9
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-17 01:54:26 +0200

    refactor(redux): use view address hook and apply it to jsx

--- src/components/User/UserAllAddress.js
+++ src/components/User/UserAllAddress.js
@@ -1,25 +1,34 @@
-import React from 'react'
-import { Row, Col } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import UserAddressCard from './UserAddressCard'
-
+import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import UserAddressCard from "./UserAddressCard";
+import ViewAddressesHook from "../../hook/user/view-addresses-hook";
 const UserAllAddress = () => {
+    const [res] = ViewAddressesHook();
     return (
         <div>
             <div className="admin-content-text pb-4">دفتر العنوانين</div>
-            <UserAddressCard />
-            <UserAddressCard />
-            <UserAddressCard />
+            {res.data ? (
+                res.data.map((item, index) => {
+                    return <UserAddressCard key={index} item={item} />;
+                })
+            ) : (
+                <h6>لا يوجد عنوانين حتى الان</h6>
+            )}
 
             <Row className="justify-content-center">
                 <Col sm="5" className="d-flex justify-content-center">
-                    <Link to="/user/add-address" style={{ textDecoration: "none" }}>
-                        <button className="btn-add-address">اضافه عنوان جديد</button>
+                    <Link
+                        to="/user/add-address"
+                        style={{ textDecoration: "none" }}
+                    >
+                        <button className="btn-add-address">
+                            اضافه عنوان جديد
+                        </button>
                     </Link>
                 </Col>
             </Row>
         </div>
-    )
-}
+    );
+};
 
-export default UserAllAddress
+export default UserAllAddress;
