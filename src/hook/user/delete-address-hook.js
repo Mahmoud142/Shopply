564cac5385dfeab505cb0625906647af6468ee4a
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-17 01:49:59 +0200

    feat: delete address hook

---
+++ src/hook/user/delete-address-hook.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { deleteUserAddress } from "./../../redux/actions/userAddressesAction";
+
+const DeleteAddressHook = (id) => {
+    const dispatch = useDispatch();
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
+    const handleDelete = async () => {
+        await dispatch(deleteUserAddress(id));
+        setShow(false);
+        window.location.reload(false);
+    };
+
+    return [show, handleClose, handleShow, handleDelete];
+};
+
+export default DeleteAddressHook;
