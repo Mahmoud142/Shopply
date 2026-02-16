c334e91475523704ef33d7ff65b7af4fb914f583
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-17 01:44:32 +0200

    feat: new userAddressesReducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -7,7 +7,7 @@ import authReducer from "./authReducer";
 import reviewReducer from "./reviewReducer";
 import addToWishListReducer from "./wishlistReducer";
 import couponReducer from "./couponReducer";
-
+import userAddressesReducer from "./userAddressesReducer";
 
 export default combineReducers({
     allCategory: categoryReducer,
@@ -18,4 +18,5 @@ export default combineReducers({
     reviewReducer: reviewReducer,
     addToWishListReducer: addToWishListReducer,
     couponReducer: couponReducer,
+    userAddressesReducer: userAddressesReducer,
 });
