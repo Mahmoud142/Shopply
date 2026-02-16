29f258ad0e3da6bcf0551b3601164d855203f5c0
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-17 00:54:42 +0200

    fix(bug): fix bug and format the code

--- src/hook/admin/view-product-admin-hook.js
+++ src/hook/admin/view-product-admin-hook.js
@@ -1,34 +1,25 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  getAllProducts,
-  getAllProductsPage,
+    getAllProducts,
+    getAllProductsPage,
 } from "../../redux/actions/productAction";
 
 const ViewProductAdminHook = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getAllProducts(10));
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(getAllProducts(10));
+    }, [dispatch]);
 
-  const onPress = async (page) => {
-    await dispatch(getAllProductsPage(page, 10));
-  };
-  let items = [];
-  let pagination = [];
+    const onPress = async (page) => {
+        await dispatch(getAllProductsPage(page, 10));
+    };
+    const products = useSelector((state) => state.allProducts.allProducts);
 
-  let products = useSelector((state) => state.allProducts.allProducts);
-  try {
-    if (products.data) {
-      items = products.data;
-    }
-    if (products.paginationResult) {
-      pagination = products.paginationResult;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-  return [items, pagination, onPress];
+    const items = products?.data ?? [];
+    const pagination = products?.paginationResult ?? [];
+
+    return [items, pagination, onPress];
 };
 export default ViewProductAdminHook;
