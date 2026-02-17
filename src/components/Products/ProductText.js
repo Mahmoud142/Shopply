bd2d75f56fbbe68de3e42b6196608e61246559cc
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-18 01:10:23 +0200

    refactor: apply add to cart hook

--- src/components/Products/ProductText.js
+++ src/components/Products/ProductText.js
@@ -1,10 +1,13 @@
-import React from "react";
 import { Row, Col } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import ViewProductDetailsHook from "../../hook/product/view-product-details-hook";
+import AddToCartHook from "../../hook/cart/add-to-cart-hook";
+import { ToastContainer } from "react-toastify";
 const ProductText = () => {
     const { id } = useParams();
     const [item, images, cat, brand] = ViewProductDetailsHook(id);
+    const [colorClick, indexColor, addToCartHandel] = AddToCartHook(id, item);
+
     return (
         <div>
             <Row className="mt-2">
@@ -35,8 +38,15 @@ const ProductText = () => {
                               return (
                                   <div
                                       key={index}
-                                      className="color ms-2 border"
-                                      style={{ backgroundColor: color }}
+                                      onClick={() => colorClick(index, color)}
+                                      className="color ms-2"
+                                      style={{
+                                          backgroundColor: color,
+                                          border:
+                                              indexColor === index
+                                                  ? "3px solid black"
+                                                  : "none",
+                                      }}
                                   ></div>
                               );
                           })
@@ -59,11 +69,15 @@ const ProductText = () => {
                     <div className="product-price d-inline px-3 py-3 border">
                         {item.price} جنية
                     </div>
-                    <div className="product-cart-add px-3 py-3 d-inline mx-3">
+                    <div
+                        onClick={addToCartHandel}
+                        className="product-cart-add px-3 py-3 d-inline mx-3"
+                    >
                         اضف للعربة
                     </div>
                 </Col>
             </Row>
+            <ToastContainer />
         </div>
     );
 };
