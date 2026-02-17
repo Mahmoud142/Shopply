60b41643442155c0998c7be4cf47f0d636356b13
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-18 01:16:27 +0200

    feat: show number of products in cart

--- src/components/utility/NavBarLogin.js
+++ src/components/utility/NavBarLogin.js
@@ -5,11 +5,12 @@ import {
   Nav,
   NavDropdown,
 } from "react-bootstrap";
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect} from "react";
 import logo from "../../images/logo.png";
 import login from "../../images/login.png";
 import cart from "../../images/cart.png";
 import NavbarSearchHook from "../../hook/search/navbar-search-hook";
+import GetAllUserCartHook from "../../hook/cart/get-all-user-cart-hook";
 const NavBarLogin = () => {
     const [onChangeSearchWord, searchWord] = NavbarSearchHook();
     let word = "";
@@ -51,63 +52,76 @@ const NavBarLogin = () => {
       setUser("");
     };
 
-  return (
-    <Navbar className="sticky-top" bg="dark" variant="dark" expand="sm">
-      <Container>
-        <Navbar.Brand>
-          <a href="/">
-            <img src={logo} className="logo" alt="E-commerce Logo" />
-          </a>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <FormControl
-            value={word}
-            onChange={onChangeSearchWord}
-            type="search"
-            placeholder="ابحث..."
-            className="me-2 w-100 text-center"
-            aria-label="Search"
-          />
-          <Nav className="me-auto">
-            {user ? (
-              <NavDropdown title={user.name} id="basic-nav-dropdown">
-                {user.role === "admin" ? (
-                  <NavDropdown.Item href="/admin/allproducts">
-                    لوحة التحكم
-                  </NavDropdown.Item>
-                ) : (
-                  <NavDropdown.Item href="/user/profile">
-                    الصفحه الشخصية
-                  </NavDropdown.Item>
-                )}
-                <NavDropdown.Divider />
-                <NavDropdown.Item onClick={logOut} href="/">
-                  تسجيل خروج
-                </NavDropdown.Item>
-              </NavDropdown>
-            ) : (
-              <Nav.Link
-                href="/login"
-                className="nav-text d-flex mt-3 justify-content-center"
-              >
-                <img src={login} className="login-img" alt="Login" />
-                <p style={{ color: "white" }}>دخول</p>
-              </Nav.Link>
-            )}
+    const [itemsNum] = GetAllUserCartHook();
 
-            <Nav.Link
-              href="/cart"
-              className="nav-text d-flex mt-3 justify-content-center"
-              style={{ color: "white" }}
-            >
-              <img src={cart} className="login-img" alt="Cart" />
-              <p style={{ color: "white" }}>العربه</p>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+
+  return (
+      <Navbar className="sticky-top" bg="dark" variant="dark" expand="sm">
+          <Container>
+              <Navbar.Brand>
+                  <a href="/">
+                      <img src={logo} className="logo" alt="E-commerce Logo" />
+                  </a>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                  <FormControl
+                      value={word}
+                      onChange={onChangeSearchWord}
+                      type="search"
+                      placeholder="ابحث..."
+                      className="me-2 w-100 text-center"
+                      aria-label="Search"
+                  />
+                  <Nav className="me-auto">
+                      {user ? (
+                          <NavDropdown
+                              title={user.name}
+                              id="basic-nav-dropdown"
+                          >
+                              {user.role === "admin" ? (
+                                  <NavDropdown.Item href="/admin/allproducts">
+                                      لوحة التحكم
+                                  </NavDropdown.Item>
+                              ) : (
+                                  <NavDropdown.Item href="/user/profile">
+                                      الصفحه الشخصية
+                                  </NavDropdown.Item>
+                              )}
+                              <NavDropdown.Divider />
+                              <NavDropdown.Item onClick={logOut} href="/">
+                                  تسجيل خروج
+                              </NavDropdown.Item>
+                          </NavDropdown>
+                      ) : (
+                          <Nav.Link
+                              href="/login"
+                              className="nav-text d-flex mt-3 justify-content-center"
+                          >
+                              <img
+                                  src={login}
+                                  className="login-img"
+                                  alt="Login"
+                              />
+                              <p style={{ color: "white" }}>دخول</p>
+                          </Nav.Link>
+                      )}
+
+                      <Nav.Link
+                          href="/cart"
+                          className="nav-text position-relative d-flex mt-3 justify-content-center"
+                          style={{ color: "white" }}
+                      >
+                          <img src={cart} className="login-img" alt="sfvs" />
+                          <p style={{ color: "white" }}>العربه</p>
+                          <span className="position-absolute top-10 start-0 translate-middle badge rounded-pill bg-danger">
+                              {itemsNum || 0}
+                          </span>
+                      </Nav.Link>
+                  </Nav>
+              </Navbar.Collapse>
+          </Container>
+      </Navbar>
   );
 };
 
