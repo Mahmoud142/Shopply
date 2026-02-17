0481aea501334e5e53c200c736ad4ee3eed4d210
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-18 01:08:03 +0200

    refactor: get all products from hook and show them in cart page

--- src/pages/Cart/CartPage.js
+++ src/pages/Cart/CartPage.js
@@ -1,28 +1,45 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import CartCheckout from '../../components/Cart/CartCheckout'
-import CartItem from '../../components/Cart/CartItem'
+import { Container, Row, Col } from "react-bootstrap";
+import CartCheckout from "../../components/Cart/CartCheckout";
+import CartItem from "../../components/Cart/CartItem";
+import GetAllUserCartHook from "./../../hook/cart/get-all-user-cart-hook";
 
 const CartPage = () => {
+    const [
+        itemsNum,
+        cartItems,
+        totalCartPrice,
+        couponNameRes,
+        totalCartPriceAfterDiscount,
+    ] = GetAllUserCartHook();
+
     return (
-        <Container style={{ minHeight: '670px' }}>
+        <Container style={{ minHeight: "670px" }}>
             <Row>
-                <div className='cart-title mt-4'>عربة التسوق</div>
+                <div className="cart-title mt-4">عربة التسوق</div>
             </Row>
-            <Row className='d-flex justify-content-center'>
+            <Row className="d-flex justify-content-center">
                 <Col xs="12" md="9">
-                    <CartItem />
-                    <CartItem />
-                    <CartItem />
-                    <CartItem />
+                    {cartItems.length >= 1 ? (
+                        cartItems.map((item, index) => {
+                            return <CartItem key={index} item={item} />;
+                        })
+                    ) : (
+                        <h6>لا يوجد منتجات فى العربة</h6>
+                    )}
                 </Col>
 
                 <Col xs="6" md="3">
-                    <CartCheckout />
+                    <CartCheckout
+                        couponNameRes={couponNameRes}
+                        totalCartPriceAfterDiscount={
+                            totalCartPriceAfterDiscount
+                        }
+                        totalCartPrice={totalCartPrice}
+                    />
                 </Col>
             </Row>
         </Container>
-    )
-}
+    );
+};
 
-export default CartPage
+export default CartPage;
