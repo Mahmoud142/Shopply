e64bcd2f43fc8dcef9db8e3ea7cf1de07012006f
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-19 01:23:57 +0200

    feat(Brand&Category): two actions for displaying al products in one (brand || category)

--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -1,151 +1,198 @@
 import { useInsertDataWithImage } from "../../hooks/useInsertData";
 import {
-  DELETE_PRODUCT,
-  UPDATE_PRODUCT,
-  CREATE_PRODUCT,
-  GET_PRODUCT_LIKE,
-  GET_ALL_PRODUCTS,
-  GET_PRODUCT_DETAILS,
-  GET_ALL_PRODUCTS_SEARCH,
-  GET_ERROR,
+    DELETE_PRODUCT,
+    UPDATE_PRODUCT,
+    CREATE_PRODUCT,
+    GET_PRODUCT_LIKE,
+    GET_ALL_PRODUCTS,
+    GET_PRODUCT_DETAILS,
+    GET_ALL_PRODUCTS_SEARCH,
+    GET_ALL_PRODUCTS_BY_CATEGORY,
+    GET_ALL_PRODUCTS_BY_BRAND,
+    GET_ERROR,
 } from "../type";
-import {useGetData} from "./../../hooks/useGetData";
+import { useGetData } from "./../../hooks/useGetData";
 import useDeleteData from "./../../hooks/useDeleteData";
 import { useUpdateDataWithImage } from "../../hooks/useUpdateData";
 
 // create product action
 export const createProduct = (formData) => async (dispatch) => {
-  try {
-    const res = await useInsertDataWithImage("/api/v1/products", formData);
-    dispatch({
-      type: CREATE_PRODUCT,
-      payload: res,
-      loading: true,
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ERROR,
-      payload: "Error  " + error,
-    });
-  }
+    try {
+        const res = await useInsertDataWithImage("/api/v1/products", formData);
+        dispatch({
+            type: CREATE_PRODUCT,
+            payload: res,
+            loading: true,
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error  " + error,
+        });
+    }
 };
 // get all products action
 export const getAllProducts = (limit) => async (dispatch) => {
-  try {
-    const res = await useGetData(`/api/v1/products?limit=${limit}`);
-    dispatch({
-      type: GET_ALL_PRODUCTS,
-      payload: res,
-      loading: true,
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ERROR,
-      payload: "Error  " + error,
-    });
-  }
+    try {
+        const res = await useGetData(`/api/v1/products?limit=${limit}`);
+        dispatch({
+            type: GET_ALL_PRODUCTS,
+            payload: res,
+            loading: true,
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error  " + error,
+        });
+    }
 };
 export const getAllProductsSearch = (queryString) => async (dispatch) => {
-  try {
-    const res = await useGetData(`/api/v1/products?${queryString}`);
-    dispatch({
-      type: GET_ALL_PRODUCTS_SEARCH,
-      payload: res,
-      loading: true,
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ERROR,
-      payload: "Error  " + error,
-    });
-  }
+    try {
+        const res = await useGetData(`/api/v1/products?${queryString}`);
+        dispatch({
+            type: GET_ALL_PRODUCTS_SEARCH,
+            payload: res,
+            loading: true,
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error  " + error,
+        });
+    }
 };
 // get all products with page number action
 export const getAllProductsPage = (page, limit) => async (dispatch) => {
-  try {
-    const res = await useGetData(
-      `/api/v1/products?page=${page}&limit=${limit}`
-    );
-    dispatch({
-      type: GET_ALL_PRODUCTS_SEARCH,
-      payload: res,
-      loading: true,
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ERROR,
-      payload: "Error  " + error,
-    });
-  }
+    try {
+        const res = await useGetData(
+            `/api/v1/products?page=${page}&limit=${limit}`,
+        );
+        dispatch({
+            type: GET_ALL_PRODUCTS_SEARCH,
+            payload: res,
+            loading: true,
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error  " + error,
+        });
+    }
 };
 
 // get one product details action
 export const getOneProduct = (id) => async (dispatch) => {
-  try {
-    const res = await useGetData(`/api/v1/products/${id}`);
-    dispatch({
-      type: GET_PRODUCT_DETAILS,
-      payload: res,
-      loading: true,
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ERROR,
-      payload: "Error  " + error,
-    });
-  }
+    try {
+        const res = await useGetData(`/api/v1/products/${id}`);
+        dispatch({
+            type: GET_PRODUCT_DETAILS,
+            payload: res,
+            loading: true,
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error  " + error,
+        });
+    }
 };
 
 // get product like action
 export const getProductLike = (id) => async (dispatch) => {
-  try {
-    const res = await useGetData(`/api/v1/products?category=${id}`);
-    dispatch({
-      type: GET_PRODUCT_LIKE,
-      payload: res,
-      loading: true,
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ERROR,
-      payload: "Error  " + error,
-    });
-  }
+    try {
+        const res = await useGetData(`/api/v1/products?category=${id}`);
+        dispatch({
+            type: GET_PRODUCT_LIKE,
+            payload: res,
+            loading: true,
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error  " + error,
+        });
+    }
 };
 
 // delete product action
 export const deleteProduct = (id) => async (dispatch) => {
-  try {
-    const res = await useDeleteData(`/api/v1/products/${id}`);
-    dispatch({
-      type: DELETE_PRODUCT,
-      payload: res,
-      loading: true,
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ERROR,
-      payload: "Error  " + error,
-    });
-  }
+    try {
+        const res = await useDeleteData(`/api/v1/products/${id}`);
+        dispatch({
+            type: DELETE_PRODUCT,
+            payload: res,
+            loading: true,
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error  " + error,
+        });
+    }
 };
 
 // update product action
 export const updateProduct = (id, formData) => async (dispatch) => {
-  try {
-    const res = await useUpdateDataWithImage(
-      `/api/v1/products/${id}`,
-      formData
-    );
-    dispatch({
-      type: UPDATE_PRODUCT,
-      payload: res,
-      loading: true,
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_ERROR,
-      payload: "Error  " + error,
-    });
-  }
+    try {
+        const res = await useUpdateDataWithImage(
+            `/api/v1/products/${id}`,
+            formData,
+        );
+        dispatch({
+            type: UPDATE_PRODUCT,
+            payload: res,
+            loading: true,
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error  " + error,
+        });
+    }
 };
+
+//get all products by category
+export const getAllProductsByCategory =
+    (page, limit, categoryID) => async (dispatch) => {
+        try {
+            const response = await useGetData(
+                `/api/v1/products?limit=${limit}&category=${categoryID}&page=${page}`,
+            );
+            console.log(
+                "response in action getAllProductsByCategory :",
+                response,
+            );
+
+            dispatch({
+                type: GET_ALL_PRODUCTS_BY_CATEGORY,
+                payload: response,
+                loading: true,
+            });
+        } catch (e) {
+            dispatch({
+                type: GET_ALL_PRODUCTS_BY_CATEGORY,
+                payload: e.response,
+            });
+        }
+    };
+
+//get all products by brand
+export const getAllProductsByBrand =
+    (page, limit, brandID) => async (dispatch) => {
+        try {
+            const response = await useGetData(
+                `/api/v1/products?limit=${limit}&brand=${brandID}&page=${page}`,
+            );
+            dispatch({
+                type: GET_ALL_PRODUCTS_BY_BRAND,
+                payload: response,
+                loading: true,
+            });
+        } catch (e) {
+            dispatch({
+                type: GET_ALL_PRODUCTS_BY_BRAND,
+                payload: e.response,
+            });
+        }
+    };
