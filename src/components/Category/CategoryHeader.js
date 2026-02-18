cf4ddf9ca812f2b277cf7b85c1ccdabc04b5cd05
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-19 01:36:23 +0200

    feat: display real categories and navigate to specific products category

--- src/components/Category/CategoryHeader.js
+++ src/components/Category/CategoryHeader.js
@@ -1,27 +1,48 @@
-import React from 'react'
-import { Container,Row ,Col} from 'react-bootstrap'
-
+import { Container, Row, Col } from "react-bootstrap";
+import AllCategoryHook from "./../../hook/category/all-category-page-hook";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 const CategoryHeader = () => {
+    const [category, loading, pageCount, getPage] = AllCategoryHook();
+
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        if (category) setItems(category.data);
+    }, [category]);
+
     return (
         <div className="cat-header">
-      <Container>
-        <Row>
-          <Col className="d-flex justify-content-start py-2 flex-wrap">
-            <div className="cat-text-header ">الكل</div>
-            <div className="cat-text-header">الكترونيات</div>
-            <div className="cat-text-header">ملابس</div>
-            <div className="cat-text-header"> كهربيه</div>
-            <div className="cat-text-header">تخفيضات</div>
-            <div className="cat-text-header">تخفيضات</div>
-            <div className="cat-text-header">تخفيضات</div>
-            <div className="cat-text-header">تخفيضات</div>
-            <div className="cat-text-header">تخفيضات</div>
-            <div className="cat-text-header">المزيد</div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-    )
-}
+            <Container>
+                <Row>
+                    <Col className="d-flex justify-content-start py-2 flex-wrap">
+                        {items
+                            ? items.map((item, index) => {
+                                  return (
+                                      <Link
+                                          to={`/products/category/${item._id}`}
+                                          style={{ textDecoration: "none" }}
+                                      >
+                                          <div
+                                              key={index}
+                                              className="cat-text-header "
+                                          >
+                                              {item.name}
+                                          </div>
+                                      </Link>
+                                  );
+                              })
+                            : null}
+                        <Link
+                            to="/allcategory"
+                            style={{ textDecoration: "none" }}
+                        >
+                            <div className="cat-text-header">المزيد</div>
+                        </Link>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
 
-export default CategoryHeader
+export default CategoryHeader;
