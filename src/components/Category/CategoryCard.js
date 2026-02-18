0ed77096fd66beb701a9703d6fae69ebf8f8aa39
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-19 01:33:29 +0200

    refactor: navigate to category when clicked & format the code

--- src/components/Category/CategoryCard.js
+++ src/components/Category/CategoryCard.js
@@ -1,25 +1,29 @@
-import React from "react";
 import { Col } from "react-bootstrap";
-
-const CategoryCard = ({ background, img, title }) => {
-  return (
-    <Col
-      xs="6"
-      sm="6"
-      md="4"
-      lg="2"
-      className="my-4 d-flex justify-content-around "
-    >
-      <div className="allCard mb-3 ">
-        <div
-          className="categoty-card "
-          style={{ backgroundColor: `${background}` }}
-        ></div>{" "}
-        <img alt="zcv" src={img} className="categoty-card-img" />
-        <p className="categoty-card-text my-2">{title}</p>
-      </div>
-    </Col>
-  );
+import { Link } from "react-router-dom";
+const CategoryCard = ({id, title, img, background }) => {
+    return (
+        <Col
+            xs="6"
+            sm="6"
+            md="4"
+            lg="2"
+            className="my-4 d-flex justify-content-around "
+        >
+            <div className="allCard mb-3 ">
+                <div
+                    className="categoty-card "
+                    style={{ backgroundColor: `${background}` }}
+                ></div>{" "}
+                <Link
+                    to={`/products/category/${id}`}
+                    style={{ textDecoration: "none" }}
+                >
+                    <img alt="zcv" src={img} className="categoty-card-img" />
+                    <p className="categoty-card-text my-2">{title}</p>
+                </Link>
+            </div>
+        </Col>
+    );
 };
 
 export default CategoryCard;
