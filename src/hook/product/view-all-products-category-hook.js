493fc36865b5ac2c100260042d63cd9367ce8469
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-02-19 01:31:27 +0200

    feat(Category): new hook for displaying all products in one category

---
+++ src/hook/product/view-all-products-category-hook.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllProductsByCategory } from "./../../redux/actions/productAction";
+const ViewAllProductsCategoryHook = (catID) => {
+    let limit = 4;
+    const dispatch = useDispatch();
+
+    const getProduct = async () => {
+        await dispatch(getAllProductsByCategory("", limit, catID));
+    };
+    useEffect(() => {
+        getProduct();
+    },[catID,dispatch]);
+
+    //when click pagination
+    const onPress = async (page) => {
+        await dispatch(getAllProductsByCategory(page, limit, catID));
+    };
+
+    const allProducts = useSelector((state) => state.allProducts?.allProductCat);
+    const items = allProducts?.data ? allProducts.data : [];
+    const pagination = allProducts?.paginationResult ? allProducts.paginationResult.numberOfPages : [];
+    return [items, pagination, onPress];
+};
+
+export default ViewAllProductsCategoryHook;
