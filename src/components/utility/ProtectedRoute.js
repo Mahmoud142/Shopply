da90bdf9c61816133b7abd4edbea074752e7035b
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-05-26 02:14:11 +0300

    style(components): update ProtectedRoute UI and logic

--- src/components/utility/ProtectedRoute.js
+++ src/components/utility/ProtectedRoute.js
@@ -1,13 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
 
-let user = localStorage.getItem("user");
-user = JSON.parse(user);
-
-const ProtectedRoute = ({ auth, children }) => {
+const ProtectedRoute = ({ allowedRole, children }) => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    
     if(user == null){
         return <Navigate to="/login" replace />;
     }
-    if (auth === false) {
+    if (allowedRole && user.role !== allowedRole) {
         return <Navigate to="/" replace />;
     }
     return children ? children : <Outlet />;
