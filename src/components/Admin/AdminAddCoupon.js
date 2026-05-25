1523dab7c75febfe89884d49a1191f670ab58260
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-05-25 15:39:00 +0300

    feat(admin): translate AdminAddCoupon.js UI text to English and align to LTR

--- src/components/Admin/AdminAddCoupon.js
+++ src/components/Admin/AdminAddCoupon.js
@@ -19,20 +19,20 @@ const AdminAddCoupon = () => {
     return (
         <div>
             <Row className="justify-content-start ">
-                <div className="admin-content-text pb-4">اضف كوبون جديد</div>
+                <div className="admin-content-text pb-4">Add New Coupon</div>
                 <Col sm="8">
                     <input
                         value={couponName}
                         onChange={onChangeName}
                         type="text"
                         className="input-form d-block mt-3 px-3"
-                        placeholder="اسم الكوبون"
+                        placeholder="Coupon Name"
                     />
                     <input
                         ref={dateRef}
                         type="text"
                         className="input-form d-block mt-3 px-3"
-                        placeholder="تاريخ الانتهاء"
+                        placeholder="Expiry Date"
                         onChange={onChangeDate}
                         value={couponDate}
                         onFocus={() => (dateRef.current.type = "date")}
@@ -43,7 +43,7 @@ const AdminAddCoupon = () => {
                         onChange={onChangeValue}
                         type="number"
                         className="input-form d-block mt-3 px-3"
-                        placeholder="نسبة خصم الكوبون"
+                        placeholder="Coupon Discount Percentage"
                     />
                 </Col>
             </Row>
@@ -53,7 +53,7 @@ const AdminAddCoupon = () => {
                         onClick={onSubmit}
                         className="btn-save d-inline mt-2 "
                     >
-                        حفظ الكوبون
+                        Save Coupon
                     </button>
                 </Col>
             </Row>
@@ -65,7 +65,7 @@ const AdminAddCoupon = () => {
                             return <AdminCouponCard key={index} coupon={item} />;
                         })
                     ) : (
-                        <h6>لا يوجد كوبونات حتى الان</h6>
+                        <h6>No coupons available</h6>
                     )}
                 </Col>
             </Row>
