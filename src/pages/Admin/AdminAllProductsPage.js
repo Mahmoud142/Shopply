9bbdb22672aa948de0801688055eaa1460980d25
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2026-05-26 02:13:54 +0300

    style(pages): update AdminAllProductsPage page UI

--- src/pages/Admin/AdminAllProductsPage.js
+++ src/pages/Admin/AdminAllProductsPage.js
@@ -17,11 +17,11 @@ const AdminAllProductsPage = () => {
   return (
     <Container>
       <Row className="py-3">
-        <Col sm="3" xs="2" md="2">
+        <Col sm="4" xs="12" md="3" lg="2" className="mb-4">
           <AdminSideBar />
         </Col>
 
-        <Col sm="9" xs="10" md="10">
+        <Col sm="8" xs="12" md="9" lg="10">
           <AdminAllProducts products={products} />
           {pageCount > 1 ? (
             <Pagination pageCount={pageCount} onPress={onPress} />
